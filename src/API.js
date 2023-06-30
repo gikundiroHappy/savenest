d4f546e0717faaad4b8348d1a20bd3ac6de7fa87
23nosurrend <keynesbizimana@gmail.com>
2023-06-30 12:11:29 +0300

    we changed the link of syclic

--- src/API.js
+++ src/API.js
@@ -1,4 +1,4 @@
-const BASE_URL="https://cloudy-wetsuit-fox.cyclic.app/";
+const BASE_URL="https://bewildered-crow-clothes.cyclic.app/";
 //const BASE_URL="http://localhost:5500"
 
 export default BASE_URL;
